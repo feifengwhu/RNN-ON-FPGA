a00a3b3535aa1509ab36bcbd5641e9c9f0289c81
Jose Fonseca <ee11126@fe.up.pt>
2016-03-27 12:21:02 +0100

    Added Remez's algorithm for minimax approximation of non-linearities

--- python/minimaxApprox/remez.py
+++ python/minimaxApprox/remez.py
@@ -1,31 +1,56 @@
 import numpy as np
+from scipy.signal import argrelmax
 
 # The target approximation function
 def f(x):
     return 1/(1+np.exp(-x))
 
 # Nber of iterations
-numIter = 10
+numIter = 5000
 
 # Approximation Interval
 b = 4
-a = -b
+a = 0
+
+# Fixed-point system precision
+prec = 2**(-10)
+t = np.arange(a, b, prec)
 
 # Polynomial Degree
 n = 3
 
 # Initial set of points
-x = np.array([(a+b)/2+(b-a)/2*np.cos(i*np.pi/(n+1)) for i in reversed(range(n+2))])
+x = np.zeros((n+2,1))
+for i in reversed(range(n+2)): x[n+1-i,0] = (a+b)/2+(b-a)/2*np.cos(i*np.pi/(n+1)) 
 
 # The numerical matrices
 A = np.zeros((n+2, n+2))
 A[:,0] = 1
 for i in range(n+2): A[i,n+1] = (-1)**(i+1)
 
-b = np.zeros((n+2,1))
-for i in range(n+2): b[i] = f(x[i])
+c = np.zeros((n+2,1))
 
 # The algorithm iterations
 for it in range(numIter):
     # Compute the A matrix    
+    for i in range(1,n+1):
+        A[:,i] = (x**i).T   
+    # Compute the b matrix
+    for i in range(n+2): c[i] = f(x[i])
+
+    # Solve the system
+    p = np.dot( np.linalg.inv(A), c )
+
+    # Recompute the x point vector
+    pol = np.poly1d(np.reshape(np.flipud(p[0:n+1]).T, (n+1)))
+    diff = abs(pol(t) - f(t))
+    extremaIndices = argrelmax(abs(diff), axis=0, mode='wrap')[0]
+    if len(extremaIndices) < (n+2) : 
+        extremaIndices = np.resize(extremaIndices, (1,n+2))
+        extremaIndices[0,n+1] = len(t)-1
+    x = t[extremaIndices].T
+     
+
     
+    
+
