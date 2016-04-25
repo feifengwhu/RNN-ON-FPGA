8ff4843e17e778cd24437cce6885f1594bfa9ff4
Jose Fonseca <ee11126@fe.up.pt>
2016-04-25 19:18:35 +0100

    Testbench file for gate that accepts real data completed

---
+++ verilog/gate/genData.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+def real_to_Qnm(real, n, m):
+    if(real >= 0):
+        return int(np.round(real*(2**m)).astype(int))
+    else:
+        return int(2**(n+m+1) + np.round(real*(2**m)).astype(int))
+def Qnm_to_real(real, n, m):
+    real = int(real) & int(2**(n+m+1)-1)
+    if(real >= 2**(n+m)):
+        return (real-2**(n+m+1))/(2**m)
+    else:
+        return real/(2**m)
+
+def sign_ext(value, newSize, oldSize):
+    if(value >= 2**(oldSize-1)):
+        return value + 2**(newSize) - 2**(oldSize)
+    else: 
+        return value
+
+NUM_MATRICES = 5
+NROW         = 16
+NCOL         = 2
+QN           = 6
+QM           = 11
+WMAX         = 7
+fin_Wx  = open('goldenIn_Wx.bin', 'w')
+fin_Wy  = open('goldenIn_Wy.bin', 'w')
+fin_b   = open('goldenIn_b.bin', 'w')
+fin_x   = open('goldenIn_x.bin', 'w')
+fin_y   = open('goldenIn_y.bin', 'w')
+fout    = open('goldenOut.bin' , 'w')
+
+quantError = 0
+
+out  = np.zeros( (NROW, 1) )
+
+for n in range(NUM_MATRICES) :
+
+    Wx  = (np.random.random( (NROW, NCOL) )-0.5)*WMAX
+    Wxq = np.zeros_like(Wx)
+    Wy  = (np.random.random( (NROW, NROW) )-0.5)*WMAX
+    Wyq = np.zeros_like(Wy)
+    x   = np.round(np.random.random( (NCOL, 1) )-0.5)
+    xq  = np.zeros_like(x)
+    y   = np.round((np.random.random( (NROW, 1) ))-0.5)
+    yq  = np.zeros_like(y)
+    b   = (np.random.random( (NROW, 1) )-0.5)*WMAX
+    bq  = np.zeros_like(b)
+
+    for i in range(NCOL):
+        xq[i,0] = real_to_Qnm(x[i,0],QN,QM)
+        fin_x.write("{0:018b}\n".format(int(xq[i,0])))
+
+    for i in range(NROW) : 
+        bq[i,0] = real_to_Qnm(b[i,0],QN,QM)
+        yq[i,0] = real_to_Qnm(y[i,0],QN,QM)
+        fin_b.write("{0:018b}\n".format(int(bq[i,0])))
+        fin_y.write("{0:018b}\n".format(int(yq[i,0])))
+        for j in range(NCOL) :
+            Wxq[i,j] = real_to_Qnm(Wx[i,j],QN,QM)
+            Wyq[i,j] = real_to_Qnm(Wy[i,j],QN,QM)
+            fin_Wx.write("{0:018b}\n".format(int(Wxq[i,j])))
+            fin_Wy.write("{0:018b}\n".format(int(Wyq[i,j])))
+
+    out  = np.dot(Wx,x) + np.dot(Wy,y) + b    
+    outq = np.zeros_like(out)
+
+    for i in range(NROW):
+        for j in range(NCOL):
+            outq[i,0] += (int(sign_ext(Wxq[i,j], 2*(QN+QM)+1,QN+QM+1) * sign_ext(xq[j,0], 2*(QN+QM)+1,QN+QM+1)/(2**QM)) & int(2**(QN+QM+1)-1))
+            outq[i,0] += (int(sign_ext(Wyq[i,j], 2*(QN+QM)+1,QN+QM+1) * sign_ext(yq[j,0], 2*(QN+QM)+1,QN+QM+1)/(2**QM)) & int(2**(QN+QM+1)-1))
+            outq[i,0] += bq[i,0]
+
+    #yq = np.dot(Wq,xq)
+    outrec = np.zeros_like(outq)
+ 
+    for i in range(NROW):
+        #yq[i,0]   = int(yq[i,0]/(2**11)) & int(0x3ffff)
+        outrec[i,0] = Qnm_to_real(outq[i,0],QN,QM)
+        #fout.write("{0:018b}\n".format(int(yq[i,0]) & int(2**(QN+QM+1)-1)))
+        fout.write("{0:018b}\n".format(real_to_Qnm(out[i,0],QN,QM)))
+    
+    quantError += sum(out-outrec)
+
+print("Quantizaion Error: ", quantError/(NUM_MATRICES*NROW))
+fin_Wx.close()
+fin_Wy.close()
+fout.close()
--- verilog/gate/wave.do
+++ verilog/gate/wave.do
@@ -1,29 +1,25 @@
 onerror {resume}
 quietly WaveActivateNextPane {} 0
-add wave -noupdate /tb_dot_prod/ROM_input
-add wave -noupdate /tb_dot_prod/colAddress_X
-add wave -noupdate /tb_dot_prod/colAddress_Y
-add wave -noupdate /tb_dot_prod/weightMem_X
-add wave -noupdate /tb_dot_prod/weightMem_Y
-add wave -noupdate /tb_dot_prod/dataReady_gate
-add wave -noupdate /tb_dot_prod/clock
-add wave -noupdate /tb_dot_prod/reset
-add wave -noupdate /tb_dot_prod/beginCalc
-add wave -noupdate /tb_dot_prod/GATE/state
-add wave -noupdate /tb_dot_prod/GATE/NEXTstate
-add wave -noupdate /tb_dot_prod/GATE/adder_X
-add wave -noupdate /tb_dot_prod/gateOutput
-add wave -noupdate /tb_dot_prod/GATE/dataReady_X
-add wave -noupdate /tb_dot_prod/GATE/dataReady_Y
-add wave -noupdate /tb_dot_prod/GATE/enable_dotprodX
-add wave -noupdate /tb_dot_prod/GATE/enable_dotprodY
-add wave -noupdate /tb_dot_prod/GATE/outputVec_X
-add wave -noupdate /tb_dot_prod/GATE/outputVec_Y
+add wave -noupdate /tb_gate/colAddressWrite_X
+add wave -noupdate /tb_gate/colAddressWrite_Y
+add wave -noupdate /tb_gate/colAddressRead_X
+add wave -noupdate /tb_gate/colAddressRead_Y
+add wave -noupdate /tb_gate/weightMemInput_X
+add wave -noupdate /tb_gate/weightMemOutput_X
+add wave -noupdate /tb_gate/weightMemInput_Y
+add wave -noupdate /tb_gate/weightMemOutput_Y
+add wave -noupdate /tb_gate/gateOutput
+add wave -noupdate /tb_gate/dataReady_gate
+add wave -noupdate /tb_gate/clock
+add wave -noupdate /tb_gate/reset
+add wave -noupdate /tb_gate/beginCalc
+add wave -noupdate /tb_gate/writeEn_X
+add wave -noupdate /tb_gate/writeEn_Y
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {41594 ps} 0}
-quietly wave cursor active 1
-configure wave -namecolwidth 318
-configure wave -valuecolwidth 100
+WaveRestoreCursors {{Cursor 1} {0 ps} 0}
+quietly wave cursor active 0
+configure wave -namecolwidth 150
+configure wave -valuecolwidth 63
 configure wave -justifyvalue left
 configure wave -signalnamewidth 0
 configure wave -snapdistance 10
@@ -34,6 +30,6 @@ configure wave -gridoffset 0
 configure wave -gridperiod 1
 configure wave -griddelta 40
 configure wave -timeline 0
-configure wave -timelineunits ps
+configure wave -timelineunits ns
 update
-WaveRestoreZoom {0 ps} {135964 ps}
+WaveRestoreZoom {0 ps} {14830 ps}
